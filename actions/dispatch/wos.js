049197295f07d74e17f8b2f44f9405abfbd1ac64
rmricketts <robertmricketts.rmr@gmail.com>
2020-03-30 18:24:57 -0700

    rricketts

--- actions/dispatch/wos.js
+++ actions/dispatch/wos.js
@@ -1,6 +1,7 @@
 'use strict';
 const Boom = require('@hapi/boom');
 const Promise = require('bluebird');
+const _ = require('lodash');
 
 module.exports.getWos = {
   method: 'GET',
@@ -14,11 +15,22 @@ module.exports.getWos = {
       return Boom.unauthorized('go fuck yaself');
     }
     let {wos} = request.server.app;
-    let {params} = request;
+    let {params, query} = request;
     let crit = {
       schedDate: {$lte: new Date(params.fromDate), $gte: new Date(params.toDate)},
       locationId: {$in: params.locations.split('/')},
     };
+    for (let key of Object.keys(query)) {
+      if (typeof query[key] === 'string' || typeof query[key] === 'number') {
+        crit[key] = query[key];
+      }
+      if (typeof query[key] === 'object') {
+        let arr = JSON.parse(query([key]));
+        if (Array.isArray(arr)) {
+          crit[key] = {$in: arr};
+        }
+      }
+    }
     let workOrders;
     try {
       workOrders = wos.find(crit).toArray();
@@ -35,15 +47,21 @@ module.exports.updateWos = {
   options: {
     pre: [require('../../modules/auth/auth.js')],
   },
-  handler: (request, h) => {
+  handler: async (request, h) => {
     let {authProfile} = request.pre;
     if (authProfile.roles.indexOf('dispatch') === -1) {
       return Boom.unauthorized('go fuck yaself');
     }
     let {payload} = request;
     let {wos} = request.server.app;
-    Promise.mapSeries(payload.updates, update => {
-      wos.updateOne({_id: update._id}, {$set: {...update.setter}});
-    });
+    try {
+      await Promise.mapSeries(payload.updates, update => {
+        wos.updateOne({_id: update._id}, {$set: {...update.updates}});
+      });
+    } catch (e) {
+      return Boom.badImplementation('Internal Error, Could not update wos');
+    }
+
+    return {success: true};
   },
 };
