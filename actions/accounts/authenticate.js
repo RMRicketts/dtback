903af37212b90b9de24a0df3eb7f70e908ca24cf
rmricketts <robertmricketts.rmr@gmail.com>
2020-04-06 22:17:16 -0700

    rricketts

--- actions/accounts/authenticate.js
+++ actions/accounts/authenticate.js
@@ -1,67 +1,77 @@
-'use strict';
-const Boom = require('@hapi/boom');
-const sign = require('../../utils/jwt.js').sign;
-const hash = require('../../utils/en.js').hash;
+"use strict";
+const Boom = require("@hapi/boom");
+const sign = require("../../utils/jwt.js").sign;
+const hash = require("../../utils/en.js").hash;
 
 module.exports.login = {
-  method: 'POST',
-  path: '/api/v1/login',
+  method: "POST",
+  path: "/api/v1/login",
   handler: async (request, h) => {
-    let {accountMembers} = request.server.app;
-    let {params, payload} = request;
+    let { accountMembers } = request.server.app;
+    let { payload } = request;
     let userProfile = await accountMembers.aggregate([
-      {$match: {userName: request.userName}},
-      {$lookup: {from: 'accounts', localField: '$accountName', foreignField: '$accountName', as: 'accountProfile'}},
-      {$project: {userName: 1, created: 1, accountName: 1, _id: 0}},
-      {$limit: 1},
+      { $match: { userName: payload.userName } },
+      {
+        $lookup: {
+          from: "accounts",
+          localField: "$accountName",
+          foreignField: "$accountName",
+          as: "accountProfile"
+        }
+      },
+      { $project: { userName: 1, created: 1, accountName: 1, _id: 0 } },
+      { $limit: 1 }
     ]);
     if (userProfile.length === 0) {
-      Boom.unathorized('Invalid login');
+      Boom.unathorized("Invalid login");
     }
     userProfile = userProfile[0];
-    let salt = userProfile.userName + userProfile.created.toString() + userProfile.accountName;
+    let salt =
+      userProfile.userName +
+      userProfile.created.toString() +
+      userProfile.accountName;
     userProfile = await accountMembers
-      .findOne({userName: request.userName, pw: hash(payload.pw, salt)})
-      .project({userName: 1, accountName: 1, roles: 1});
+      .findOne({ userName: payload.userName, pw: hash(payload.pw, salt) })
+      .project({ userName: 1, accountName: 1, roles: 1 });
     if (userProfile === null) {
-      Boom.unauthorized('Invalid login');
+      Boom.unauthorized("Invalid login");
     }
     const token = sign(userProfile);
     return token;
-  },
+  }
 };
 
 module.exports.auth = {
-  method: 'POST',
-  path: '/api/v1/auth',
+  method: "POST",
+  path: "/api/v1/auth",
   handler: async (request, h) => {
-    let {employees} = request.server.app;
-    let {params, payload} = request;
+    let { employees } = request.server.app;
+    let { payload } = request;
     let salt = bdy.userName + bdy.accountName + bdy.created.toString();
     let profile = await employees
-      .findOne({userName: request.userName})
-      .project({userName: 1, created: 1, accountName: 1, _id: 0});
+      .findOne({ userName: payload.userName })
+      .project({ userName: 1, created: 1, accountName: 1, _id: 0 });
     if (userProfile.length === 0) {
-      Boom.unathorized('Invalid login');
+      Boom.unathorized("Invalid login");
     }
     profile = userProfile[0];
     let salt = userProfile.userName + userProfile.created.toString();
     profile = await employees
-      .findOne({userName: request.userName, pw: hash(payload.pw, salt)})
-      .project({userName: 1, accountName: 1, roles: 1, _id: 1});
+      .findOne({ userName: payload.userName, pw: hash(payload.pw, salt) })
+      .project({ userName: 1, accountName: 1, roles: 1, _id: 1 });
     if (userProfile === null) {
-      Boom.unauthorized('Invalid login');
+      Boom.unauthorized("Invalid login");
     }
     const token = sign(userProfile);
     return token;
-  },
+  }
 };
 
 module.exports.oauth = {
-  method: 'POST',
-  path: '/api/v1/oauth',
+  method: "POST",
+  path: "/api/v1/oauth",
   handler: async (request, h) => {
-    let {employees} = request.server.app;
-    let {params} = request;
-  },
+    let { employees } = request.server.app;
+    let { payload } = request;
+  }
 };
